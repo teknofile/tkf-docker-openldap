034a3513292f2f9f4e42f91200b71b2845452c4e
teknofile - James Richardson <teknofile@users.noreply.github.com>
2019-07-18 13:47:47 -0600

    Delete kill.sh

--- kill.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-docker stop tkf-openldap
-docker rm tkf-openldap
-
-rm -rf test-work/openldap-conf/ test-work/openldap-data/ test-work/slapd.log
-rm -rf test-work/data/ test-work/openldap/
